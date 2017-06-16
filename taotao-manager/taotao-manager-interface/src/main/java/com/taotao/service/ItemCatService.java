feab2e74c9ac5127bb351df5603c71217b849ab6
zengxingbin <2045620125@qq.com>
2017-06-16 15:15:26 +0800

    clear repository

--- taotao-manager/taotao-manager-interface/src/main/java/com/taotao/service/ItemCatService.java
+++
@@ -1,9 +0,0 @@
-package com.taotao.service;
-
-import java.util.List;
-
-import com.taotao.common.pojo.EasyUITreeNode;
-
-public interface ItemCatService {
-    public List<EasyUITreeNode> getTbItemCatList(long parentId);
-}
