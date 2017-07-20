0f7ee0e785f2b2d6f3417e11d95fac1db34fb6b8
zengxingbin <2045620125@qq.com>
2017-07-20 16:13:28 +0800

    update taotao-content

--- taotao-content/taotao-content-service/src/main/java/com/taotao/content/service/impl/ContentServiceImpl.java
+++ taotao-content/taotao-content-service/src/main/java/com/taotao/content/service/impl/ContentServiceImpl.java
@@ -5,7 +5,9 @@ import java.util.Collections;
 import java.util.Date;
 import java.util.List;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import com.github.pagehelper.PageHelper;
@@ -13,14 +15,21 @@ import com.github.pagehelper.PageInfo;
 import com.taotao.common.pojo.EasyUIDatagridResult;
 import com.taotao.common.pojo.TaotaoResult;
 import com.taotao.content.service.ContentService;
+import com.taotao.jedis.JedisClient;
 import com.taotao.mapper.TbContentMapper;
 import com.taotao.pojo.TbContent;
 import com.taotao.pojo.TbContentExample;
 import com.taotao.pojo.TbContentExample.Criteria;
+import com.taotao.utils.JsonUtils;
 @Service
 public class ContentServiceImpl implements ContentService {
     @Autowired
     private TbContentMapper tbContentMapper;
+    //注入jedisClient
+    @Autowired
+    private JedisClient jedisClient;
+    @Value("${INDEX_CONTENT}")
+    private String INDEX_CONTENT;
     @Override
     public EasyUIDatagridResult queryContent(long id, int page, int rows) {
         //设置分页信息
@@ -47,6 +56,12 @@ public class ContentServiceImpl implements ContentService {
         content.setUpdated(new Date());
         //插入到数据库中
         tbContentMapper.insert(content);
+      //同步缓存,把数据库中修改过的内容从缓存里删了
+        try {
+            jedisClient.hdel(INDEX_CONTENT, content.getCategoryId()+"");
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
         return TaotaoResult.ok();
     }
     @Override
@@ -55,7 +70,12 @@ public class ContentServiceImpl implements ContentService {
         content.setUpdated(new Date());
         //更新到到数据库中
         tbContentMapper.updateByPrimaryKey(content);
-        
+        //同步缓存,把数据库中修改过的内容从缓存里删了
+        try {
+            jedisClient.hdel(INDEX_CONTENT, content.getCategoryId()+"");
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
         return TaotaoResult.ok();
     }
     @Override
@@ -70,11 +90,28 @@ public class ContentServiceImpl implements ContentService {
     }
     @Override
     public List<TbContent> geTbContentListByCid(long cid) {
+        //查询前先查询缓存，查询缓存异常时不能影响正常的业务逻辑，所以要try。。。catch
+        try {
+            String list = jedisClient.hget(INDEX_CONTENT,cid + "");
+            if(StringUtils.isNotBlank(list)) {//如果字符串不为空
+                //转换成list集合返回
+                List<TbContent> tbContentList = JsonUtils.jsonToList(list, TbContent.class);
+                return tbContentList;
+            }
+        } catch (Exception e) {
+            System.out.println("Operation Exception!");
+        }
         //创建查询条件
         TbContentExample example = new TbContentExample();
         Criteria criteria = example.createCriteria();
         criteria.andCategoryIdEqualTo(cid);
         List<TbContent> list = tbContentMapper.selectByExample(example);
+        //把从数据库中查询到的内容添加到缓存中
+        try {
+            jedisClient.hset(INDEX_CONTENT, cid + "",JsonUtils.objectToJson(list));
+        } catch (Exception e) {
+            System.out.println("Operation exception!");
+        }
         return list;
     }
 
